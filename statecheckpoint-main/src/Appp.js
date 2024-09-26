23e9ab067db23bdcbb90d3483196d7c3ff925d14
yasselghoul <yassinellghoull06@gmail.com>
2024-09-26 16:10:27 +0100

    state

---
+++ statecheckpoint-main/src/Appp.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    constructor(props){
+     super(props);
+     this.state =
+    {
+      person : {
+    fullName:"Mohamed khalfaoui Aziz",
+      bio:"i'm developer",
+      imgSrc:"aziz.jpg",
+      profession:"FullStack Developer",
+    },
+
+      show:true,
+      timer:0,
+      intervall:null,
+    }
+    Show=()=>{
+        this.setState({
+          show: !this.state.show
+        })
+      }
+      
+}
+componentDidMount(){
+    console.log('hello')
+    this.setState({
+      intervall:setInterval(
+        ()=>({timer: this.state.timer +1}),
+        1000
+      )
+    })
+
+  };
+render(){
+    const { person, show } = this.state;
+    return(
+    <>
+      <button onClick={(this.Show)}>{show ? "Hide" : "Show"} Profile</button>
+      
+    {this.state.show && (
+      <>
+      <img src={person.imgSrc} alt ={person.fullName}/>
+      <h1>{person.fullName}</h1>
+      <p>{person.bio}</p>
+      <p>{person.profession}</p>
+      
+      </>
+      
+    )}
+   <p>{this.state.timer}</p>
+    </>
+
+)}
+}
